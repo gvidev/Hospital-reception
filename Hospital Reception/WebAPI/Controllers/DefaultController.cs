137d580caf33e534196db4ff5eab9f1dd378d818
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2023-05-17 14:19:51 +0300

    adding WebAPI project

---
+++ Hospital Reception/WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: Default
+        [HttpGet]
+        public IHttpActionResult Version()
+        {
+            return Json("API Version 1.0");
+        }
+    }
+}
